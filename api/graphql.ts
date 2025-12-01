93299fe10040757bebfdc1c48b6d61196eaecf95
Daksh Khanna <115382295+DakshK26@users.noreply.github.com>
2025-12-01 18:55:11 -0500

    Graph QL Api Layer Modification

--- api/graphql.ts
+++ api/graphql.ts
@@ -12,7 +12,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true,
-  context: ({ req }) => ({
+  context: ({ req }: { req: any }) => ({
     // Pass headers or auth tokens here
     headers: req.headers
   })
@@ -26,7 +26,7 @@ export const config = {
   },
 };
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
